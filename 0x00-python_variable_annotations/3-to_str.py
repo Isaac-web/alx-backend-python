63d0661b34d008502be3664f8861a5779bcf532b
Isaac Takiy <kanytakiy@gmail.com>
2023-02-02 16:39:30 +0000

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Contains a function that returns a string
+annotation of a number.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating point number
+    to a string
+    """
+    return str(n)
