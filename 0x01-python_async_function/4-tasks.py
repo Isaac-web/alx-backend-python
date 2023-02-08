ec03e788c07024c9a9e862c493b525a60676644f
Isaac-web <kanytakiy@gmail.com>
2023-02-08 16:46:58 +0000

    Fix Task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,5 +9,8 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n multiple times"""
-    return await asyncio.gather(
+    items = await asyncio.gather(
         *(task_wait_random(max_delay) for _ in range(n)))
+
+    items.sort()
+    return items
